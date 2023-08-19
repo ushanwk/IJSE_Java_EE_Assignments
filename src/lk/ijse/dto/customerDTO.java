884161fc2d520392683e4f2176f5055e81e2c2bc
UshanKaushalya <Ushanwk22@gmail.com>
2023-08-19 21:11:52 +0530

    Second commit

---
+++ src/lk/ijse/dto/customerDTO.java
@@ -0,0 +1,32 @@
+package lk.ijse.dto;
+
+public class customerDTO {
+
+    private String customerId;
+    private String customerName;
+    private String customerAddress;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+}
---
+++ src/lk/ijse/servlet/customerServlet.java
@@ -0,0 +1,23 @@
+package lk.ijse.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/pages/customer")
+public class customerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+    }
+
+}
